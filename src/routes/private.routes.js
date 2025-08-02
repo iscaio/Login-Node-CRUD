eba5a67eb572f1d8034b6d356793d4923247bc83
Caio Andrade <Contatoiscaio@gmail.com>
2025-08-02 14:28:08 -0300

    Update private.routes.js

--- src/routes/private.routes.js
+++ src/routes/private.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const UserController = require("../controllers/user.controller");
 const router = express.Router();
 
-//Routes
 router.get("/users", UserController.allUsers);
 router.get("/user/:id", UserController.getById);
 router.patch("/update-user/:id", UserController.updateUser);
